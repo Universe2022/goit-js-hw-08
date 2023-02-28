366c95df98f56b9e89f2847aad4c7983a06b66ad
Universe2022 <svitlankagarna05@gmail.com>
2023-02-28 23:56:02 +0200

    update

---
+++ src/js/01-gallery.js
@@ -0,0 +1,42 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+function imageHTML(largeImageJpg, smallImageJpg, imageDescription) {
+  const str = `
+                    <li class="gallery__item">
+                        <a class="gallery__link" href="${largeImageJpg}">
+                            <img
+                                class="gallery__image" 
+                                src="${smallImageJpg}" 
+                                alt="${imageDescription}"
+                            />
+                        </a>
+                    </li>         
+                `;
+  return str;
+}
+
+//Main
+
+const refs = {
+  galleryUl: document.querySelector('ul.gallery'),
+};
+
+galleryItems.map(item =>
+  refs.galleryUl.insertAdjacentHTML(
+    'beforeend',
+    imageHTML(item.original, item.preview, item.description)
+  )
+);
+
+let lightbox = new SimpleLightbox('ul.gallery a.gallery__link', {
+  captions: true,
+  captionSelector: 'img',
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
