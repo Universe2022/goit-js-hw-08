00899a7b2dc957eff8008b945486cb40f8753ee7
Universe2022 <svitlankagarna05@gmail.com>
2023-03-05 12:16:48 +0200

    update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
+//Import
 
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
+
+// Data
+
+const time1 = 1000;
+const keyVideoplayerCurrentTime = 'videoplayer-current-time';
+const iframe = document.querySelector('iframe#vimeo-player');
+
+// Functions
+
+function onTimeUpdate(event) {
+  localStorage.setItem(keyVideoplayerCurrentTime, `${event.seconds}`);
+  console.log(
+    'Current time = ' + localStorage.getItem(keyVideoplayerCurrentTime)
+  );
+}
+
+// Main
+
+if (!localStorage.getItem(keyVideoplayerCurrentTime)) {
+  localStorage.setItem(keyVideoplayerCurrentTime, '0');
+}
+
+const player = new Player(iframe);
+
+player.setCurrentTime(localStorage.getItem(keyVideoplayerCurrentTime));
+
+player.on('timeupdate', throttle(onTimeUpdate, time1));
